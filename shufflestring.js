39cf92f916cebd857df090b218fb6cc1f8cf710e
James McCormack <thejamesmccormack@gmail.com>
2017-08-18 09:38:30 -0700

    updated js file to es6 standards

--- shufflestring.js
+++ shufflestring.js
@@ -1,21 +1,19 @@
-function james(str){
-// declare some variables
+function james(str) {
+  // where the final output lives.
+  const strArray = str.toLowerCase().split(''); // changing initial string to an array
+  const a = strArray.length; // storing the length of the array
 
-var shuffledStr; // where the final output lives.
-var strArray = str.toLowerCase().split(''); // changing initial string to an array
-var randomIndex; // variable to store the random index generated each time.
-var a = strArray.length; // storing the length of the array
+  // this loop generates a random number and turns it into the index of strArray
+  // so that we can cycle through and shuffle.
+  for (let i = a - 1; i > 0; i -= 1) {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    const temp = strArray[i];
+    strArray[i] = strArray[randomIndex];
+    strArray[randomIndex] = temp;
+  }
 
-// this loop generates a random number and turns it into the index of strArray
-// so that we can cycle through and shuffle.
-for (var i = a - 1; i > 0; i--) {
-  var randomIndex = Math.floor(Math.random() * (i + 1));
-  var temp = strArray[i];
-  strArray[i] = strArray[randomIndex];
-  strArray[randomIndex] = temp;
-};
-
-  // join the results
-  shuffledStr = strArray.join("");
+  const shuffledStr = strArray.join('');
   return shuffledStr;
 }
+
+james('james');
